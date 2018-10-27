5c6873340d4bffd4797b18761e1a6bffbdce1ae7
shikd <shikd@bjzdgt.com>
2018-10-27 06:52:48 +0000

    init shell

--- germ/init.sh
+++ germ/init.sh
@@ -1,2 +1,2 @@
 #/usr/bin/env bash
-ansible-playbook -i inventory/inventory.cfg -e @../etc/kolla/globals.yml -e @../etc/kolla/passwords.yml /home/kolla/dire/steam/ansible/sit.yml --limit control-2 -t interfaces
+ansible-playbook -i inventory/inventory.cfg -e @../etc/kolla/globals.yml -e @../etc/kolla/passwords.yml /home/kolla/dire/steam/ansible/sit.yml
