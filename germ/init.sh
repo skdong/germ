7747e067f35b54dbffc661f04d9e629ef7c56be1
shikd <shikd@bjzdgt.com>
2018-10-30 02:25:27 +0000

    pass openstack deploy test

--- germ/init.sh
+++ germ/init.sh
@@ -1,2 +1,2 @@
 #/usr/bin/env bash
-ansible-playbook -i inventory/inventory.cfg -e @../etc/kolla/globals.yml -e @../etc/kolla/passwords.yml /home/kolla/dire/steam/ansible/sit.yml
+ansible-playbook -e @../etc/kolla/globals.yml sit.yml -t cluster $*
