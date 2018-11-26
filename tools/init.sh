a0cb8d8c96c0b852fb03cc3f88800ae898113135
zl.zhang <zl.zhang@slancer.com>
2018-11-26 18:34:18 +0800

    modify param

--- tools/init.sh
+++ tools/init.sh
@@ -2,7 +2,7 @@
 
 MODULE=$(dirname $(readlink -f $0))
 
-python $MODULE/generate_passwords.py -f $MODULE/../etc/germ/passwords.yml
+python $MODULE/generate_passwords.py -p $MODULE/../etc/germ/passwords.yml
 
 ansible-playbook $MODULE/init.yml
 chmod 0600 $MODULE/../etc/germ/id_rsa
