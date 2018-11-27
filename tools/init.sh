23ede97e105972da326dc3f5f273c153b84605b6
19930711sx <995201057@qq.com>
2018-11-27 10:49:59 +0000

    modify config

--- tools/init.sh
+++ tools/init.sh
@@ -2,13 +2,21 @@
 
 MODULE=$(dirname $(readlink -f $0))
 
+case "$1" in
+    "k8s")
+        cp $MODULE/../germ/ansible-k8s.cfg $MODULE/../germ/ansible.cfg
+        ;;
+    "openstack")
+        cp $MODULE/../germ/ansible-openstack.cfg $MODULE/../germ/ansible.cfg
+        ;;
+    *)
+        echo "Parameter error.For example: k8s or openstack"
+        exit 1
+        ;;
+esac
+
 python $MODULE/generate_passwords.py -p $MODULE/../etc/germ/passwords.yml
 
-ansible-playbook $MODULE/init.yml
+ansible-playbook $MODULE/init.yml -e germ_path=$MODULE/..
 chmod 0600 $MODULE/../etc/germ/id_rsa
 
-if [ "$1"x = "k8s"x ] ;then
-    cp $MODULE/../germ/ansible-k8s.cfg $MODULE/../germ/ansible.cfg
-else
-    cp $MODULE/../germ/ansible-openstack.cfg $MODULE/../germ/ansible.cfg
-fi
