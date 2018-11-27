cad73b7008d84235ac65cb52253e37360410f096
zl.zhang <zl.zhang@slancer.com>
2018-11-27 17:36:11 +0800

    modify config

--- tools/init.sh
+++ tools/init.sh
@@ -6,3 +6,8 @@ python $MODULE/generate_passwords.py -p $MODULE/../etc/germ/passwords.yml
 
 ansible-playbook $MODULE/init.yml
 chmod 0600 $MODULE/../etc/germ/id_rsa
+if ["$1" = "k8s" ] ;then
+    mv -f ../germ/ansible-k8s.cfg ../germ/ansible.cfg
+else
+    mv -f ../germ/ansible-openstack.cfg ../germ/ansible.cfg
+fi
